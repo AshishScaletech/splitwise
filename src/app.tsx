8997f17b2603c9eafaf240bf6799eb10ad41b115
AshishScaletech <ashish.patel@scaletech.xyz>
2023-07-04 11:47:56 +0530

    initial commit

--- src/app.tsx
+++ src/app.tsx
@@ -8,28 +8,38 @@ import ResetPassword from 'features/auth/component/resetPassword';
 
 import { IState } from 'shared/interface/state';
 import Layout from 'hoc/layout/layout';
+import Home from 'features/split/container/home';
 
 const App: React.FC = () => {
 	const isLogin: boolean = useSelector((state: IState) => state.auth.isLogin);
 
-	if (isLogin) {
-		return (
-			<Layout>
-				<Routes>
-					<Route path='/' />
-				</Routes>
-			</Layout>
-		);
-	} else {
-		return (
+	return (
+		<Layout>
 			<Routes>
-				<Route path='/login' element={<Login />} />
-				<Route path='/forgot-password' element={<ForgotPassword />} />
-				<Route path='/reset-password/:token' element={<ResetPassword />} />
-				<Route path='*' element={<Navigate replace to='/login' />} />
+				<Route path='/home' element={<Home />} />
+				<Route path='*' element={<Navigate replace to='/home' />} />
 			</Routes>
-		);
-	}
+		</Layout>
+	)
+
+	// if (isLogin) {
+	// 	return (
+	// 		<Layout>
+	// 			<Routes>
+	// 				<Route path='/' />
+	// 			</Routes>
+	// 		</Layout>
+	// 	);
+	// } else {
+	// 	return (
+	// 		<Routes>
+	// 			<Route path='/login' element={<Login />} />
+	// 			<Route path='/forgot-password' element={<ForgotPassword />} />
+	// 			<Route path='/reset-password/:token' element={<ResetPassword />} />
+	// 			<Route path='*' element={<Navigate replace to='/login' />} />
+	// 		</Routes>
+	// 	);
+	// }
 };
 
 export default App;
